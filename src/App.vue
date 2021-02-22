70d25de557cdbceb48a443596939b373902da642
arjun-sudo <arjungautam5431@gmail.com>
2021-02-22 17:51:35 +0545

    input flex box for todo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
+    <Header/>
+    <AddTodo/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import Header from './components/layout/Header'
 import Todos from './components/Todos.vue';
+import AddTodo from "@/components/AddTodo";
 
 export default {
   name: "App",
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
   },
   data() {
     return {
@@ -55,5 +61,19 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form>
+      <input type="text" name="title" placeholder="Add Todo..">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo"
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 5px
+}
+
+input[type="submit"] {
+  flex: 2;
+}
+
+</style>
